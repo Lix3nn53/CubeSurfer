4b68221171f1a858ec49ff5563925234035dc376
Lix3nn53 <lixakyildiz@gmail.com>
2022-02-06 13:37:46 +0300

    Auto calculate data for obstacle

--- Assets/Scripts/Triggerable/Obstacle.cs
+++ Assets/Scripts/Triggerable/Obstacle.cs
@@ -10,31 +10,36 @@ public class Obstacle : Triggerable
         public int height;
     }
 
-    [SerializeField] private ObstaclePart[] line0 = {};
-    [SerializeField] private ObstaclePart[] line1 = {};
-    [SerializeField] private ObstaclePart[] line2 = {};
-    [SerializeField] private ObstaclePart[] line3 = {};
-    [SerializeField] private ObstaclePart[] line4 = {};
+    [SerializeField] private ObstaclePart[][] lines;
 
     public override void OnTrigger(Collider other) {
         var go = other.gameObject;
         if (go == null || !other.gameObject.CompareTag("Player"))
             return;
 
-        ObstaclePart[] parts;
-        int line = PlayerMovement.Instance.Line;
-        if (line == 0) {
-            parts = line0;
-        } else if (line == 1) {
-            parts = line1;
-        } else if (line == 2) {
-            parts = line2;
-        } else if (line == 3) {
-            parts = line3;
-        } else {
-            parts = line4;
-        }
+        ObstaclePart[] parts = lines[PlayerMovement.Instance.Line];
 
         PlayerCollider.Instance.OnObstacle(parts);
+    }
+
+    void Awake() {
+        AutoFillLineData();
+    }
+
+    private void AutoFillLineData() {
+        lines = new ObstaclePart[5][];
+        for (int i = 0; i < 5; i++) {
+            Transform obstacleLine = transform.GetChild(i);
+            int lineIndex = (int) obstacleLine.localPosition.z + 2;
+
+            int obstacleCount = obstacleLine.childCount;
+            lines[i] = new ObstaclePart[obstacleCount];
+            for (int y = 0; y < obstacleCount; y++) {
+                Transform obstacle = obstacleLine.GetChild(y);
+
+                ObstaclePart obstaclePart = new ObstaclePart() { start = (int) obstacle.localPosition.y, height = (int) obstacle.localScale.y };
+                lines[i][y] = obstaclePart;
+            }
+        }
     }
 }
