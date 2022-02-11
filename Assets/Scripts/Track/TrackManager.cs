11641acfdc1c56d0ebcf25f9eabd213ce12d2b8f
Lix3nn53 <lixakyildiz@gmail.com>
2022-02-12 02:47:56 +0300

    Add more segments

--- Assets/Scripts/Track/TrackManager.cs
+++ Assets/Scripts/Track/TrackManager.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class TrackManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] trackPrefab;
-    private GameObject before; // -50
-    private GameObject current; // 0
-    private GameObject next; // 50
+    [SerializeField] private float trackLength = 100;
 
-    private int index = 1;
+    GameObject[] array = new GameObject[6];
 
     private void Awake() {
-        this.current = this.trackPrefab[0];
-        this.current.transform.position = new Vector3(0, 0, 0);
-        this.next = this.trackPrefab[1];
-        this.next.transform.position = new Vector3(50, 0, 0);
-        this.before = this.trackPrefab[2];
-        this.before.transform.position = new Vector3(-50, 0, 0);
+        this.array[0] = this.trackPrefab[0];
+        this.array[0].transform.position = new Vector3(-trackLength, 0, 0);
+        this.array[1] = this.trackPrefab[1];
+        this.array[1].transform.position = new Vector3(0, 0, 0);
+        this.array[2] = this.trackPrefab[2];
+        this.array[2].transform.position = new Vector3(trackLength, 0, 0);
+        this.array[3] = this.trackPrefab[3];
+        this.array[3].transform.position = new Vector3(trackLength * 2, 0, 0);
+        this.array[4] = this.trackPrefab[4];
+        this.array[4].transform.position = new Vector3(trackLength * 3, 0, 0);
+        this.array[5] = this.trackPrefab[5];
+        this.array[5].transform.position = new Vector3(trackLength * 4, 0, 0);
     }
     
     void FixedUpdate()
@@ -25,21 +29,24 @@ public class TrackManager : MonoBehaviour
         Vector3 position = PlayerMovement.Instance.transform.position;
         float distance = position.x;
 
-        if (distance > 50) {
+        if (distance > trackLength) {
             PlayerMovement.Instance.transform.position = new Vector3(0, position.y, position.z);
-            this.before = this.current;
-            this.before.transform.position = new Vector3(-50, 0, 0);
-            this.current = this.next;
-            this.current.transform.position = new Vector3(0, 0, 0);
 
-            if (index == trackPrefab.Length - 1) {
-                index = 0;
-            } else {
-                index++;
-            }
+            GameObject temp = this.array[0];
 
-            this.next = this.trackPrefab[index];
-            this.next.transform.position = new Vector3(50, 0, 0);
+            this.array[0] = this.array[1];
+            this.array[0].transform.position = new Vector3(-trackLength, 0, 0);
+            this.array[1] = this.array[2];
+            this.array[1].transform.position = new Vector3(0, 0, 0);
+            this.array[2] = this.array[3];
+            this.array[2].transform.position = new Vector3(trackLength, 0, 0);
+            this.array[3] = this.array[4];
+            this.array[3].transform.position = new Vector3(trackLength * 2, 0, 0);
+            this.array[4] = this.array[5];
+            this.array[4].transform.position = new Vector3(trackLength * 3, 0, 0);
+
+            this.array[5] = temp;
+            this.array[5].transform.position = new Vector3(trackLength * 4, 0, 0);
         }
     }
 }
