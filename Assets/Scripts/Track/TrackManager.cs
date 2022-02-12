e23774cf68768ecba5fcedf829ffe053c1a6f1ff
Lix3nn53 <lixakyildiz@gmail.com>
2022-02-12 10:47:21 +0300

    refactor trackmanager

--- Assets/Scripts/Track/TrackManager.cs
+++ Assets/Scripts/Track/TrackManager.cs
@@ -7,21 +7,16 @@ public class TrackManager : MonoBehaviour
     [SerializeField] private GameObject[] trackPrefab;
     [SerializeField] private float trackLength = 100;
 
-    GameObject[] array = new GameObject[6];
+    private GameObject[] trackBuffer;
 
     private void Awake() {
-        this.array[0] = this.trackPrefab[0];
-        this.array[0].transform.position = new Vector3(-trackLength, 0, 0);
-        this.array[1] = this.trackPrefab[1];
-        this.array[1].transform.position = new Vector3(0, 0, 0);
-        this.array[2] = this.trackPrefab[2];
-        this.array[2].transform.position = new Vector3(trackLength, 0, 0);
-        this.array[3] = this.trackPrefab[3];
-        this.array[3].transform.position = new Vector3(trackLength * 2, 0, 0);
-        this.array[4] = this.trackPrefab[4];
-        this.array[4].transform.position = new Vector3(trackLength * 3, 0, 0);
-        this.array[5] = this.trackPrefab[5];
-        this.array[5].transform.position = new Vector3(trackLength * 4, 0, 0);
+        this.trackBuffer = new GameObject[trackPrefab.Length];
+
+        for (int i = 0; i < trackBuffer.Length; i++) {
+            this.trackBuffer[i] = this.trackPrefab[i];
+
+            this.trackBuffer[i].transform.position = new Vector3(-this.trackLength + (i * this.trackLength), 0, 0);
+        }
     }
     
     void FixedUpdate()
@@ -32,21 +27,16 @@ public class TrackManager : MonoBehaviour
         if (distance > trackLength) {
             PlayerMovement.Instance.transform.position = new Vector3(0, position.y, position.z);
 
-            GameObject temp = this.array[0];
+            GameObject temp = this.trackBuffer[0];
 
-            this.array[0] = this.array[1];
-            this.array[0].transform.position = new Vector3(-trackLength, 0, 0);
-            this.array[1] = this.array[2];
-            this.array[1].transform.position = new Vector3(0, 0, 0);
-            this.array[2] = this.array[3];
-            this.array[2].transform.position = new Vector3(trackLength, 0, 0);
-            this.array[3] = this.array[4];
-            this.array[3].transform.position = new Vector3(trackLength * 2, 0, 0);
-            this.array[4] = this.array[5];
-            this.array[4].transform.position = new Vector3(trackLength * 3, 0, 0);
+            for (int i = 0; i < trackBuffer.Length - 1; i++) {
+                this.trackBuffer[i] = this.trackBuffer[i + 1];
 
-            this.array[5] = temp;
-            this.array[5].transform.position = new Vector3(trackLength * 4, 0, 0);
+                this.trackBuffer[i].transform.position = new Vector3(-this.trackLength + (i * this.trackLength), 0, 0);
+            }
+
+            this.trackBuffer[trackBuffer.Length - 1] = temp;
+            this.trackBuffer[trackBuffer.Length - 1].transform.position = new Vector3(-this.trackLength + (trackLength * (trackBuffer.Length - 1)), 0, 0);
         }
     }
 }
